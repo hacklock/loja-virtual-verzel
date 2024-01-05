8b2503f2fa990c91002916071727da5b9a0a576c
Wilton R. Cruz <jiquitaiasoftware@gmail.com>
2024-01-05 19:55:47 -0300

    Subindo aplicação

---
+++ src/main/java/br/com/kavak/LojaVirtualVerzelApplication.java
@@ -0,0 +1,13 @@
+package br.com.kavak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualVerzelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualVerzelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kavak/LojaVirtualVerzelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kavak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualVerzelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
