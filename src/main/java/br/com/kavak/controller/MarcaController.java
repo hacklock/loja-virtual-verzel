23e02c806ab55ed6090290c2569701ffe21fbd65
Wilton R. Cruz <jiquitaiasoftware@gmail.com>
2024-01-06 13:58:10 -0300

    Alterando as classes

--- src/main/java/br/com/kavak/controller/MarcaController.java
+++ src/main/java/br/com/kavak/controller/MarcaController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,19 +31,19 @@ public class MarcaController {
 	}
 
 	@PostMapping("/")
-	public Marca inserir(Marca marca) {
+	public Marca inserir(@RequestBody Marca marca) {
 		return marcaService.inserir(marca);
 
 	}
 
 	@PutMapping("/")
-	public Marca alterar(Marca marca) {
+	public Marca alterar(@RequestBody Marca marca) {
 		return marcaService.alterar(marca);
 
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> excluir(@PathParam("id")Long id) {
+	public ResponseEntity<Void> excluir(@PathVariable("id")Long id) {
 		marcaService.excluir(id);
 		return ResponseEntity.ok().build();
 
